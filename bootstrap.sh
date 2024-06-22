bc0c1605021f86e3274695f19aed30031ca0b059
Telain <telain000@gmail.com>
2024-06-22 14:52:24 -0500

    Update bootstrap.sh
    Remove custom directory in favor of local

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,41 +1,6 @@
 #!/bin/sh
-# Setting up config.ini
-if [ ! -L "/usr/local/share/asphyxia/config.ini" ] && [ -f "/usr/local/share/asphyxia/config.ini" ]; then
-	echo "LOG: Default config.ini FOUND; Moving default config.ini"
-	mv /usr/local/share/asphyxia/config.ini /usr/local/share/asphyxia/config_default.ini
-fi
-if test -f "/usr/local/share/custom/config.ini"; then
-	echo "LOG: Custom config.ini FOUND; Setting up symbolic link to custom config.ini"
-	ln -sf /usr/local/share/custom/config.ini /usr/local/share/asphyxia/config.ini
-else
-	echo "LOG: custom config.ini NOT FOUND; Setting up symbolic link to default config.ini"
-	ln -sf /usr/local/share/asphyxia/config_default.ini /usr/local/share/asphyxia/config.ini 
-fi
-
 # Setting up savedata
-SAVE_DIR=""
-if [ -d "/usr/local/share/custom/savedata" ]; then
-	echo "LOG: Custom savedata FOUND"
-	SAVE_DIR="-d /usr/local/share/custom/savedata"
-else
-	echo "LOG: Custom savedata NOT FOUND; Using default savedata"
-fi
-
-# Setting up plugins
-rm -rf /usr/local/share/asphyxia/plugins/*
-if [ -d "/usr/local/share/custom/plugins" ]; then
-    if [ ! -z "${ASPHYXIA_PLUGIN_REPLACE}" ]; then
-        echo "LOG: ASPHYXIA_PLUGIN_REPLACE defined, not copying default plugins"
-    else
-        echo "LOG: Adding default plugins"
-        cp -r /usr/local/share/asphyxia/plugins_default/* /usr/local/share/asphyxia/plugins/
-    fi
-	echo "LOG: Custom plugins FOUND; Adding custom plugins"
-	cp -r /usr/local/share/custom/plugins/* /usr/local/share/asphyxia/plugins/
-else
-    echo "LOG: Custom plugins NOT FOUND; Adding default plugins"
-    cp -r /usr/local/share/asphyxia/plugins_default/* /usr/local/share/asphyxia/plugins/
-fi
+SAVE_DIR="-d /usr/local/share/asphyxia/savedata"
 
 [ ! -z "${ASPHYXIA_LISTENING_PORT}" ] && LISTENING_PORT="--port ${ASPHYXIA_LISTENING_PORT}" || LISTENING_PORT=""
 [ ! -z "${ASPHYXIA_BINDING_HOST}" ] && BINDING_PORT="--bind ${ASPHYXIA_BINDING_HOST}" || BINDING_PORT=""
@@ -43,7 +8,6 @@ fi
 [ ! -z "${ASPHYXIA_DEV_MODE}" ] && DEV_MODE="--dev" || DEV_MODE=""
 [ ! -z "${ASPHYXIA_PING_IP}" ] && PING_IP="--ping-addr ${ASPHYXIA_PING_IP}" || PING_IP=""
 [ ! -z "${ASPHYXIA_SAVEDATA_DIR}" ] && SAVEDATA_DIR="--savedata-dir ${ASPHYXIA_SAVEDATA_DIR}" || SAVEDATA_DIR=""
-
 
 echo "RUNNING: /usr/local/share/asphyxia/asphyxia-core ${SAVE_DIR} ${LISTENING_PORT} ${BINDING_PORT} ${MATCHING_PORT} ${DEV_MODE} ${PING_IP} ${SAVEDATA_DIR}"
 /usr/local/share/asphyxia/asphyxia-core ${SAVE_DIR} ${LISTENING_PORT} ${BINDING_PORT} ${MATCHING_PORT} ${DEV_MODE} ${PING_IP} ${SAVEDATA_DIR}
